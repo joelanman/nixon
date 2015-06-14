1cc0e7da588f2832434698cd71579a8192a85044
Edward Horsford <ed.horsford@digital.cabinet-office.gov.uk>
2015-06-14 11:32:27 +0100

    Update readme with new sizes functionality

--- scripts/example.js
+++ scripts/example.js
@@ -4,9 +4,34 @@ module.exports = {
     username: "",
     password: "",
 
-    sizes : [
-        [1024,768],
-        [320,480]
+    sizes: [
+        {
+            width: 320
+        },
+        {
+            width: 1024,
+        },
+        {
+            name: "iPad",
+            width: 768,
+            zoom: 2,
+            suffix: '_x2'
+        },
+        {
+            name: "Page headings",
+            width: 1024,
+            crop: 'h1'
+        },
+        {
+            name: "Square crop",
+            width: 1024,
+            crop: {
+                top : 100,
+                left: 100,
+                width: 100,
+                height: 100
+            }
+        }
     ],
 
     steps : {
